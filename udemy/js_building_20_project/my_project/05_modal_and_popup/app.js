018e9ae7f07b69b4268f59c52d429d7cc5d34b0c
vapun <vaapunn@gmail.com>
2024-06-03 17:16:14 +0700

    05_finish

--- udemy/js_building_20_project/my_project/05_modal_and_popup/app.js
+++ udemy/js_building_20_project/my_project/05_modal_and_popup/app.js
@@ -1,3 +1,11 @@
 const openBtn = document.querySelector(".open-btn")
 const closeBtn = document.querySelector(".close-btn")
-const modalConainer = document.querySelector(".modal-cintaainer") 
+const modalContainer = document.querySelector(".modal-container") 
+
+openBtn.addEventListener("click",()=>{
+    modalContainer.classList.add("show")
+})
+
+closeBtn.addEventListener("click",()=>{
+    modalContainer.classList.remove("show")
+})
