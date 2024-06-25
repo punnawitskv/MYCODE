83ebd08ed41641e0b908fac5926f849ffcdda8f1
vapun <vaapunn@gmail.com>
2024-06-25 20:52:41 +0700

    sa

--- other/udemy/js_building_20_project/my_project/15_hidden_search_widget/app.js
+++ other/udemy/js_building_20_project/my_project/15_hidden_search_widget/app.js
@@ -5,5 +5,11 @@ const btn = document.querySelector(".btn")
 btn.addEventListener("click", ()=>{
     // console.log("test");
     search.classList.toggle("active")
-    input.focus()
+    // input.focus()
+    if (search.classList.contains("active")) {
+        input.removeAttribute("readonly");
+        input.focus();
+    } else {
+        input.setAttribute("readonly", "readonly");
+    }
 })
