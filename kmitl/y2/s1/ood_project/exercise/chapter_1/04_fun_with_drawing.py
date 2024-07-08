49cd8eeeb37b930e4bb726fb8263ac56d0cef453
Valentine <vaapunn@gmail.com>
2024-07-08 10:43:05 +0700

    .

--- kmitl/y2/s1/ood_project/exercise/chapter_1/04_fun_with_drawing.py
+++ kmitl/y2/s1/ood_project/exercise/chapter_1/04_fun_with_drawing.py
@@ -12,7 +12,7 @@ def draw_pyramid(size):
         for x in range(count_for_loop * -1, count_for_loop + 1, 1):
             
             map_num = 0
-
+            
             if abs(x) > abs(y):
                 map_num = abs(x)
             else:
