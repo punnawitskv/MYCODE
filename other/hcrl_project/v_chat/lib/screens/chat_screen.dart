1a576959831faf38d46c9ba6a29978d148927047
punnawit <punnawitsukhumvada@gmail.com>
2024-09-25 22:30:57 +0700

    34

--- other/hcrl_project/v_chat/lib/screens/chat_screen.dart
+++ other/hcrl_project/v_chat/lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:v_chat/api/apis.dart';
 import 'package:v_chat/main.dart';
 import 'package:v_chat/models/chat_user.dart';
 
@@ -22,8 +23,53 @@ class _ChatScreenState extends State<ChatScreen> {
           automaticallyImplyLeading: false,
           flexibleSpace: _appBar(),
         ),
+
+        // body
         body: Column(
-          children: [_chatInput()],
+          children: [
+            Expanded(
+              child: StreamBuilder(
+                // stream: APIs.getAllUsers(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    // loading
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                    // return const Center(child: CircularProgressIndicator());
+
+                    // loaded
+                    case ConnectionState.active:
+                    case ConnectionState.done:
+                      // final data = snapshot.data?.docs;
+                      // _list =
+                      //     data?.map((e) => ChatUser.fromJson(e.data())).toList() ??
+                      //         [];
+
+                      final _list = ['hi', 'hello'];
+
+                      if (_list.isNotEmpty) {
+                        return ListView.builder(
+                            itemCount: _list.length,
+                            padding: EdgeInsets.only(top: mq.height * .01),
+                            physics: const BouncingScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              return Text('Message: ${_list[index]}');
+                            });
+                      } else {
+                        return const Center(
+                          child: Text(
+                            'Say Hi! 👋',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        );
+                      }
+                  }
+                },
+                stream: null,
+              ),
+            ),
+            _chatInput(),
+          ],
         ),
       ),
     );
